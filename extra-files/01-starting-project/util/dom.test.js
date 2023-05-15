254e7359890e35c591807de2095f5702dccd5037
oi <105376328+delfo2@users.noreply.github.com>
2023-05-15 19:27:19 -0300

    doing last test into dom.js

--- extra-files/01-starting-project/util/dom.test.js
+++ extra-files/01-starting-project/util/dom.test.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import {it, expect, vi} from 'vitest';
+import {it, expect, vi, beforeEach} from 'vitest';
 import { Window } from 'happy-dom'
 
 import { showError } from './dom';
@@ -10,9 +10,34 @@ const HtmlDocContent = fs.readFileSync(HtmlDocPath).toString();
 
 const window = new Window();
 const document = window.document;
-document.write(HtmlDocContent);
 vi.stubGlobal('document', document);
 
-it('first test ', () => {
+beforeEach(() => {
+    document.body.innerHTML = '';
+    document.write(HtmlDocContent);
+})
+
+it('should create a paragraph element in div"errors"', () => {
     showError('test');
+    const divEl = document.getElementById('errors');
+    const paragraphDiv = divEl.firstElementChild;
+
+    expect(paragraphDiv).not.toBeNull();
+});
+
+it('should not contain an error paragraph in div with id="errors"', () => {
+    const divEl = document.getElementById('errors');
+    const paragraphDiv = divEl.firstElementChild;
+
+    expect(paragraphDiv).toBeNull();
+});
+
+it('should contain an error message with provided message', () => {
+    const errorMessage = 'test';
+
+    showError(errorMessage);
+    const divEl = document.getElementById('errors');
+    const paragraphDiv = divEl.firstElementChild;
+
+    expect(paragraphDiv.textContent).toBe(errorMessage);
 });
